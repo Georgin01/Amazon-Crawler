124b9cb77c96d4a3cdfc5c9c1301c4e45e42447e
Galay Yuriy <y4109galay@gmail.com>
2020-02-24 11:03:31 +0200

    Create basic request function

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const axios = require('axios');
 
-function axioss(url) {
-    axios.get(url)
-        .then(response => console.log(response.data));
+const basicUrl = 'https://www.amazon.com/s';
+
+function recursiveCrawling(pageNum = 1, arrResults = []) {
+    return axios.get(basicUrl, {
+        params: {k: 'laptop', page: pageNum},
+        headers: {'X-Requested-With': 'XMLHttpRequest'}
+    })
+        .then(response => {
+            return response.data;
+        })
+        .then(data => {
+            //process some data
+        })
+        .catch(err => {
+           console.error(err);
+           return arrResults;
+        });
 }
 
-axioss(`https://www.amazon.com/s?k=laptop&page=2`);
+recursiveCrawling();
