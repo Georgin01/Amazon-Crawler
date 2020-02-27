a8e53959e9b32d60f3b79c0247524817b5ae5dff
Galay Yuriy <y4109galay@gmail.com>
2020-02-27 20:50:15 +0200

    done the cheerio handler func

--- app.js
+++ app.js
@@ -9,15 +9,24 @@ function cheerioHandler(html){
     const $ = cheerio.load(html);
 
     $('.s-result-item').each((i, elem) => {
+        let link = $(elem).find('.s-border-bottom .a-spacing-medium .sg-row:nth-child(2) div:nth-child(2) .sg-row:first-child a').attr('href');
 
-            linksOfProducts.push($(elem).data('index'));
+        //Filtering an advertising links and concat with basic url
+        if (link && !link.match(/advertising/g)){
+            link = basicUrl.replace('/s', link);
+            linksOfProducts.push(link);
+        }
+    });
 
-    });//linksOfProducts.push(elem));
+    //if button 'NEXT' has class 'a-disabled' (we don`t have nextPage link) - 'hasNextPage' == true
+    //So, we use the '!' sign to get false. it is more logical.
+    let hasNextPage = !$('.a-spacing-none .a-last').hasClass('a-disabled');
 
-    return linksOfProducts;
+    return {links: linksOfProducts,
+            next: hasNextPage};
 }
-
-function recursiveCrawling(pageNum = 1, arrResults = []) {
+        
+function recursiveCrawling(pageNum = 20, arrResults = []) {
     return axios.get(basicUrl, {
         params: {k: 'laptop', page: pageNum},
         headers: {'X-Requested-With': 'XMLHttpRequest'}
