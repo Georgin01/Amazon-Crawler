a35025f132dc1422ffe42551e29f55f3d1d780e6
Galay Yuriy <y4109galay@gmail.com>
2020-02-28 09:50:39 +0200

    create cheerioUtil module

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
+
+import {getLinksCheerio, getProductDataCheerio} from "./cheerioUtil";
 
 const basicUrl = 'https://www.amazon.com/s';
 
-function cheerioHandler(html){
-    let linksOfProducts = [];
-
-    const $ = cheerio.load(html);
-
-    $('.s-result-item').each((i, elem) => {
-        let link = $(elem).find('.s-border-bottom .a-spacing-medium .sg-row:nth-child(2) div:nth-child(2) .sg-row:first-child a').attr('href');
-
-        //Filtering an advertising links and concat with basic url
-        if (link && !link.match(/advertising/g)){
-            link = basicUrl.replace('/s', link);
-            linksOfProducts.push(link);
-        }
-    });
-
-    //if button 'NEXT' has class 'a-disabled' (we don`t have nextPage link) - 'hasNextPage' == true
-    //So, we use the '!' sign to get false. it is more logical.
-    let hasNextPage = !$('.a-spacing-none .a-last').hasClass('a-disabled');
-
-    return {links: linksOfProducts,
-            next: hasNextPage};
-}
-        
 function recursiveCrawling(pageNum = 1, arrResults = []) {
     return axios.get(basicUrl, {
         params: {k: 'laptop', page: pageNum},
@@ -36,7 +14,7 @@ function recursiveCrawling(pageNum = 1, arrResults = []) {
         })
         .then(data => {
             console.log(pageNum); //delete in final
-            let processedInfo = cheerioHandler(data);
+            let processedInfo = getLinksCheerio(data);
 
             if (processedInfo.links) arrResults = arrResults.concat(processedInfo.links);
             if (processedInfo.next) return recursiveCrawling(pageNum+1, arrResults);
---
+++ cheerioUtil.js
@@ -0,0 +1,35 @@
+const cheerio = require('cheerio');
+
+//we use this function to get links of product
+function getLinksCheerio(html){
+    let linksOfProducts = [];
+
+    const $ = cheerio.load(html);
+
+    $('.s-result-item').each((i, elem) => {
+        let link = $(elem).find('.s-border-bottom .a-spacing-medium .sg-row:nth-child(2) div:nth-child(2) .sg-row:first-child a').attr('href');
+
+        //Filtering an advertising links and concat with basic url
+        if (link && !link.match(/advertising/g)){
+            link = basicUrl.replace('/s', link);
+            linksOfProducts.push(link);
+        }
+    });
+
+    //if button 'NEXT' has class 'a-disabled' (we don`t have nextPage link) - 'hasNextPage' == true
+    //So, we use the '!' sign to get false. it is more logical.
+    let hasNextPage = !$('.a-spacing-none .a-last').hasClass('a-disabled');
+
+    return {links: linksOfProducts,
+        next: hasNextPage};
+}
+
+//we use 'getProductDataCheerio' to get info from product page
+function getProductDataCheerio(html) {
+
+}
+
+module.exports = {
+    getLinksCheerio,
+    getProductDataCheerio
+};
