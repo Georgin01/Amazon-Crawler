6a58bbf2d0ecf32ee7d75048371d0a95e1f814f6
Galay Yuriy <y4109galay@gmail.com>
2020-02-29 17:41:34 +0200

    using TaskQueue

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const {recursiveLinkCrawling, getProductData} = require('./requestingUtil');
 
+const TaskQueue = require('./taskQueue');
 
-// function crawler(){
-//     return recursiveLinkCrawling()
-//         .then(links => {
-//             let promises = links.map(link => getProductData(link));
-//
-//             return Promise.all(promises);
-//         })
-//         .catch(err => console.log(err));
-// }
-//
-// crawler().then(data => console.log(data));
+let queue = new TaskQueue(2);
+
+async function crawler() {
+    let links = await recursiveLinkCrawling();
+
+    if (links.length === 0){
+        return Promise.resolve();
+    }
+
+    return new Promise((res, rej) => {
+        let completed = 0;
+
+        links.forEach(link => {
+           let task = () => {
+                return getProductData(link)
+                    .then(() => {
+                        if (++completed === links.length) res();
+                    })
+                    .catch((err) => {
+                         if (err) rej();
+                    });
+           };
+           queue.pushTask(task);
+        });
+    });
+}
+
+crawler().then(data => console.log(data));
